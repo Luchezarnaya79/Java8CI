dadf3853ff29a8614bcd51cff8db29fc7bb77447
Ольга Петрова <luchezarnaya79olga@yandex.ru>
2022-10-29 20:28:33 +0300

    С тестами

---
+++ src/main/java/ru/netology/statistic/Main.java
@@ -0,0 +1,12 @@
+package ru.netology.statistic;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
+        System.out.println("Максимум =" + service.findMax(incomesInBillions));
+
+    }
+}
